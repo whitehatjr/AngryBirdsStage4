81ba6b7d7c49cc5e98ae0df322f737d16e3ca82e
TJ907 <81124510+TJ907@users.noreply.github.com>
2021-04-15 20:42:41 -0400

    Update Slingshot.js

--- Slingshot.js
+++ Slingshot.js
@@ -16,12 +16,26 @@ class SlingShot{
     }
 
     display(){
+        image(this.sling1,200,20);
+        image(this.sling2,170,20);
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
+            push()
             strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            stroke(48,22,8);
+            if (pointA.x<220){
+                line(pointA.x-25, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x+25, pointA.y, pointB.x+30, pointB.y-3);
+                image(this.sling3, pointA.x-30,pointA.y-10,15,30);
+            }else{
+            line(pointA.x-25, pointA.y, pointB.x-10, pointB.y);
+            line(pointA.x+25, pointA.y, pointB.x+30, pointB.y-3);
+            image(this.sling3, pointA.x+25,pointA.y-10,15,30);
+            }
+            pop()
         }
     }
+    
     
 }
