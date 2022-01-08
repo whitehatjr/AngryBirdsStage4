00e78597856ca6e65e48b6819f41f75f6a13c58d
giannah00 <87567114+giannah00@users.noreply.github.com>
2022-01-08 14:28:10 +0530

    Update Slingshot.js

--- Slingshot.js
+++ Slingshot.js
@@ -16,11 +16,16 @@ class SlingShot{
     }
 
     display(){
-        if(this.sling.bodyA){
+        image(this.sling1,200,20);
+        image(this.sling2,170,20);
+            if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
             strokeWeight(4);
+            stroke(48,22,8);
             line(pointA.x, pointA.y, pointB.x, pointB.y);
+            line(pointA.x - 20. pointA.y, pointB.x + 30, pointB.y - 3);
+                pop();
         }
     }
     
